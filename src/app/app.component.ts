19967083eb1d646534df2688f6c2c3a28a5759fb
Henrique Silva <henrique.rrs@hotmail.com>
2019-02-25 15:01:07 -0300

    Atualizando tudo do curso da udemy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-graphcool-chat';
+
+  private apiUrl = 'https://api.graph.cool/simple/v1/cjskfnd3k27m901883l1ot3v1';
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.createUser();
+    this.allUsers();
+  }
+  allUsers(): void {
+    const body = {
+      query: `
+      query {
+        allUsers {
+          id
+          name
+          email
+        }
+      }
+      `
+    };
+
+    this.http.post(this.apiUrl, body)
+      .subscribe(res => console.log('query: ', res));
+  }
+
+
+  createUser(): void {
+    const body = {
+      query: `
+        mutation CreateNewUsers($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name: 'Henrique',
+        email: 'ihenriclick@gmail.com',
+        password: '123456'
+      }
+    };
+
+    this.http.post(this.apiUrl, body)
+    .subscribe(res => console.log('Mutation: ', res));
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { ApolloConfigModule } from './apollo-config.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ApolloConfigModule,
+    HttpClientModule,
+    MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
