0ee663520880e2d958b6242d32f1374e8724325b
Henrique Silva <henrique.rrs@hotmail.com>
2019-03-06 08:12:32 -0300

    AuthGuard add a aplicação

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,24 +1,37 @@
 import { AUTHENTICATE_USER_MUTATION, SIGNUP_USER_MUTATION } from './auth.graphql';
 import { Apollo } from 'apollo-angular';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, ReplaySubject, throwError } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private _isAuthenticated = new ReplaySubject<boolean>(1);
+
   constructor(
     private apollo: Apollo
-  ) {}
+  ) {
+    this.isAuthenticated.subscribe(is => console.log('authstate', is));
+  }
+
+  get isAuthenticated(): Observable<boolean> {
+    return this._isAuthenticated.asObservable();
+  }
 
   entrarUser(variables: {email: string, password: string}): Observable<{id: string, token: string}> {
     return this.apollo.mutate({
       mutation: AUTHENTICATE_USER_MUTATION,
       variables
     }).pipe(
-      map(res => res.data.authenticateUser)
+      map(res => res.data.authenticateUser),
+      tap(res => this.setAuthState(res !== null)),
+      catchError(err => {
+        this.setAuthState(false);
+        return throwError(err);
+      })
     );
   }
 
@@ -27,8 +40,13 @@ export class AuthService {
       mutation: SIGNUP_USER_MUTATION,
       variables
     }).pipe(
-      map(res => res.data.criarUser)
+      map(res => res.data.criarUser),
+      tap(res => this.setAuthState(res !== null))
     );
+  }
+
+  private setAuthState(isAuthenticated: boolean): void {
+    this._isAuthenticated.next(isAuthenticated);
   }
 
 }
---
+++ src/app/login/auth.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  CanActivateChild,
+  CanLoad,
+  Route
+} from '@angular/router';
+import { LoginRoutingModule } from './login-routing.module';
+import { AuthService } from '../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+@Injectable({ providedIn: LoginRoutingModule })
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.isAuthenticated;
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.canActivate(route, state);
+  }
+
+  canLoad(route: Route): Observable<boolean> {
+    return this.authService.isAuthenticated
+    .pipe(take(1));
+  }
+}
