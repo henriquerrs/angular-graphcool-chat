d79c983394151e0594cc217d0e4b4450d7077fdb
Henrique Silva <henrique.rrs@hotmail.com>
2019-03-08 13:27:02 -0300

    Chat module em processo

---
+++ src/app/core/services/user.graphql.ts
@@ -0,0 +1,16 @@
+import { User } from '../models/user.model';
+import gql from 'graphql-tag';
+
+export interface AllUsersQuery {
+  allUsers: User[];
+}
+
+export const ALL_USERS_QUERY = gql`
+    query AllUsersQuery {
+      allUsers(orderBy: name_ASC) {
+        id
+        name
+        email
+      }
+    }
+`;
