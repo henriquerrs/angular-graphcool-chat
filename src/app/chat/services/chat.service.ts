77f54f42250ad339d53d03e85bf30fb79512412e
Henrique Silva <henrique.rrs@hotmail.com>
2019-03-12 09:03:19 -0300

    getUserChats está pronto para uso

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ChatModule } from '../chat.module';
+import { Observable } from 'rxjs';
+import { Chat } from '../models/chat.model';
+import { Apollo } from 'apollo-angular';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: ChatModule
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo,
+    private authService: AuthService
+  ) { }
+
+  getUserChats(): Observable<Chat[]> {
+    return this.apollo.query<AllChatsQuery>({
+      query: USER_CHATS_QUERY,
+      variables: {
+        userId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => res.data.allChats)
+    );
+  }
 }
