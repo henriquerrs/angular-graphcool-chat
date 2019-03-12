cbcec0ab24fd506da07996468a95d1b6a48b9126
Henrique Silva <henrique.rrs@hotmail.com>
2019-03-12 13:19:39 -0300

    resolver para chat-window

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Chat } from '../models/chat.model';
 import { Apollo } from 'apollo-angular';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { AllChatsQuery, USER_CHATS_QUERY, ChatQuery, CHAT_BY_ID_OR_BY_USERS_QUERY } from './chat.graphql';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -25,6 +25,19 @@ export class ChatService {
       }
     }).pipe(
       map(res => res.data.allChats)
+    );
+  }
+
+  getChatByIdOrByUsers(chatOrUserId: string): Observable<Chat> {
+  return this.apollo.query<ChatQuery | AllChatsQuery>({
+      query: CHAT_BY_ID_OR_BY_USERS_QUERY,
+      variables: {
+        chatId: chatOrUserId,
+        targetUserId: chatOrUserId,
+        loggedUserId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => (res.data['Chat']) ? res.data['Chat'] : res.data['allChats'][0])
     );
   }
 }
