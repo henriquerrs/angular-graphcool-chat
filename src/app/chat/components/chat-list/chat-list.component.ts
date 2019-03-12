c6cf0cfdac32cc50858d971fba08d77f7cea157c
Henrique Silva <henrique.rrs@hotmail.com>
2019-03-12 10:37:36 -0300

    chat-list corrigindo template

--- src/app/chat/components/chat-list/chat-list.component.ts
+++ src/app/chat/components/chat-list/chat-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AllChatsQuery } from '../../services/chat.graphql';
+import { Chat } from '../../models/chat.model';
+import { ChatService } from '../../services/chat.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-chat-list',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatListComponent implements OnInit {
 
-  constructor() { }
+  chats$: Observable<Chat[]>;
+
+  constructor(
+    private authService: AuthService,
+    private chatService: ChatService
+  ) { }
 
   ngOnInit() {
+    this.chats$ = this.chatService.getUserChats();
   }
 
+  getChatTitle(chat: Chat): string {
+    return chat.title || chat.users[0].name;
+  }
+
+  getLastMessage(chat: Chat): string {
+    const message = chat.messages[0];
+    if (message) {
+      const sender =
+      (message.sender.id === this.authService.authUser.id)
+        ? 'Você'
+        : message.sender.name;
+      return `${sender}: ${message.text}`;
+    }
+    return 'Sem Messagens.';
+  }
 }
